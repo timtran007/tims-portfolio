85d61f7fd09bf80364c0543dc8557afceb2234ac
Tim <timtran007@gmail.com>
2023-04-26 01:09:20 -0500

    updates layout to remove old styling

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link'
+import Navbar from './navbar';
 
 
 
@@ -12,7 +11,7 @@ export const siteTitle = "Tim Tran's Portfolio Site"
 
 export default function Layout({ children, home }) {
     return (
-        <div className={styles.container}>
+        <div>
             <Head>
                 <link rel='icon' href='/favicon.ico'/>
             <meta
@@ -26,45 +25,20 @@ export default function Layout({ children, home }) {
             <meta name="og:title" content={siteTitle} />
             <meta name="twitter:card" content="summary_large_image" />
             </Head>
-            <header className={styles.header}>
+            <header>
                 {home ? (
                     <>
-                        <Image
-                            priority
-                            src='/images/tims-profile-pic.jpg'
-                            className={utilStyles.borderCircle}
-                            height={144}
-                            width={144}
-                            alt="Tim"
-                        />
-                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                        <Navbar />
                     </> 
                     ) : (
                         <>
-                            <Link href='/'>
-                                <Image
-                                    priority
-                                    src="/images/tims-profile-pic.jpg"
-                                    className={utilStyles.borderCircle}
-                                    height={108}
-                                    width={108}
-                                    alt="Tim"
-                                />
-                            </Link>
-                            <h2 className={utilStyles.headingLg}>
-                                <Link href='/' className={utilStyles.colorInherit}>
-                                    {name}
-                                </Link>
-                            </h2>
+                            <Navbar />
+                                
                         </>
                 )}
             </header>
             <main>{children}</main>
-            {!home && (
-                <div className={styles.backToHome}>
-                    <Link href="/">← Back to home</Link>
-                </div>
-            )}
+           
         </div>
     )
 }
