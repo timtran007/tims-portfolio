d1332537e8c18550fd2c79fbdd900ce137949851
Tim <timtran007@gmail.com>
2023-04-29 02:05:19 -0500

    updates blog layout

--- pages/blog.js
+++ pages/blog.js
@@ -1,31 +1,40 @@
 import Layout, { siteTitle } from "../components/layout"
 import Head from 'next/head';
-import utilStyles from '../styles/utils.module.css'
 import { getSortedPostData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
 
 export default function Blog({ allPostsData }) {
     return (
-        <Layout>
-            <Head>
-                <title>{siteTitle}</title>
-            </Head>
-            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-                <h2 className={utilStyles.headingLg}>Blog</h2>
-                <ul className={utilStyles.lists}>
-                {allPostsData.map(({ id, date, title }) => (
-                    <li className={utilStyles.listItem} key={id}>
-                    <Link href={`/posts/${id}`}>{title}</Link>
-                    <br />
-                    <small className={utilStyles.lightText}>
-                        <Date dateString={date}/>
-                    </small>
-                    </li>
-                ))}
-                </ul>
-            </section>
-        </Layout>
+        <>
+            <Layout>
+                <Head>
+                    {siteTitle}
+                </Head>
+            </Layout>
+            <div className='w-full lg:h-screen p-2'>
+                <div className='py-4 max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
+                    <p className='pt-16 text-xl tracking-widest uppercase text-[#1DB954]'>Blog</p>
+                    <h2 className='py-4'>What I Am Learning</h2>
+                        <ul className='grid md:grid-cols-2 lg:grid-cols-2 gap-8'>
+                        {allPostsData.map(({ id, date, title }) => (
+                            <div className='p-4 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                                <div className='p-2 grid grid-cols-1 gap-4 justif'>
+                                    <li key={id}>
+                                    <Link href={`/posts/${id}`}>{title}</Link>
+                                    <br />
+                                    <small>
+                                        <Date dateString={date}/>
+                                    </small>
+                                    </li>
+                                </div>
+                            </div>
+                        ))}
+                        </ul>
+                </div>
+            </div>
+        </>
+        
     )
 }
 
