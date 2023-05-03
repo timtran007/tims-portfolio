dc9b015e215e7ba72d1cc51e4bba0f39963eeaad
Tim <timtran007@gmail.com>
2023-05-03 02:16:26 -0500

    fixes warning about map needing unique key

--- pages/blog.js
+++ pages/blog.js
@@ -6,7 +6,7 @@ import Date from '../components/date';
 
 export default function Blog({ allPostsData }) {
     return (
-        <>
+        <div>
             <Layout>
                 <Head>
                     {siteTitle}
@@ -16,24 +16,24 @@ export default function Blog({ allPostsData }) {
                 <div className='py-4 max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
                     <p className='pt-16 text-xl tracking-widest uppercase text-[#1DB954]'>Blog</p>
                     <h2 className='py-4'>What I Am Learning</h2>
-                        <ul className='grid md:grid-cols-2 lg:grid-cols-2 gap-8'>
-                        {allPostsData.map(({ id, date, title }) => (
-                            <div className='p-4 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                                <div className='p-2 grid grid-cols-1 gap-4 justif'>
-                                    <li key={id}>
-                                    <Link href={`/posts/${id}`}>{title}</Link>
-                                    <br />
-                                    <small>
-                                        <Date dateString={date}/>
-                                    </small>
-                                    </li>
+                        <div className='grid md:grid-cols-2 lg:grid-cols-2 gap-8'>
+                            {allPostsData.map(({ id, date, title }) => (
+                                <div key={id} className='p-4 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                                    <ul className='p-2 grid grid-cols-1 gap-4 justif'>
+                                        <li key={id}>
+                                            <Link href={`/posts/${id}`}>{title}</Link>
+                                            <br />
+                                            <small>
+                                                <Date dateString={date}/>
+                                            </small>
+                                        </li>
+                                    </ul>
                                 </div>
-                            </div>
-                        ))}
-                        </ul>
+                            ))}
+                        </div>
                 </div>
             </div>
-        </>
+        </div>
         
     )
 }
