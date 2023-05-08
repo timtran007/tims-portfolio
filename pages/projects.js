915706a19fc61d3b5f43f5fd17233cc2e23aec4a
Tim <timtran007@gmail.com>
2023-05-08 09:18:01 -0500

    updates project links

--- pages/projects.js
+++ pages/projects.js
@@ -23,13 +23,13 @@ function Projects() {
                 <p className='py-6 text-xl tracking-widest uppercase text-[#1DB954]'>Projects</p>
                 <h2 className='pb-8'>What I've Built</h2>
                 <div className='grid md:grid-cols-2 gap-8'>
-                    <ProjectItem title='Meal Time App' appImage={mealTimeImage} projectUrl='/meal-time' techStack='React, Rails & PostgreSQL'/>
-                    <ProjectItem title='Foodies App' appImage={foodiesImage} projectUrl='/foodies' techStack='React, Rails & PostgreSQL' />
-                    <ProjectItem title='Dog Walking HTX Website' appImage={dogWalkingImage} projectUrl='/dog-walking-htx' techStack='WordPress, Elementor & Hostinger'/>
-                    <ProjectItem title='Decisive Digital Marketing Website' appImage={decisiveDigitalMarketingImage} projectUrl='/ddm' techStack='WordPress, Elementor & HostGator'/>
-                    <ProjectItem title='Dog Adventures App' appImage={dogAdventuresImage} projectUrl='/dog-adventures' techStack='React, Sinatra & SQLite'/>
-                    <ProjectItem title='Trip Save App' appImage={tripSaveImage} projectUrl='/trip-save' techStack='React'/>
-                    <ProjectItem title='On Recall App' appImage={onRecallImage} projectUrl='/on-recall' techStack='HTML, CSS & JavaScript'/>
+                    <ProjectItem title='Meal Time App' appImage={mealTimeImage} projectUrl='/projects/meal-time' techStack='React, Rails & PostgreSQL'/>
+                    <ProjectItem title='Foodies App' appImage={foodiesImage} projectUrl='/projects/foodies' techStack='React, Rails & PostgreSQL' />
+                    <ProjectItem title='Dog Walking HTX Website' appImage={dogWalkingImage} projectUrl='/projects/dog-walking-htx' techStack='WordPress, Elementor & Hostinger'/>
+                    <ProjectItem title='Decisive Digital Marketing Website' appImage={decisiveDigitalMarketingImage} projectUrl='/projects/ddm' techStack='WordPress, Elementor & HostGator'/>
+                    <ProjectItem title='Dog Adventures App' appImage={dogAdventuresImage} projectUrl='/projects/dog-adventures' techStack='React, Sinatra & SQLite'/>
+                    <ProjectItem title='Trip Save App' appImage={tripSaveImage} projectUrl='/projects/trip-save' techStack='React'/>
+                    <ProjectItem title='On Recall App' appImage={onRecallImage} projectUrl='/projects/on-recall' techStack='HTML, CSS & JavaScript'/>
                 </div>
             </div>
         </div>
