e2a1816cc96d1b55276835799b6f02030e4be1a5
Tim <timtran007@gmail.com>
2023-05-08 11:12:19 -0500

    adds navbar in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import Navbar from '../components/navbar'
 import '../styles/globals.css'
+
 
 export default function App({ Component, pageProps}) {
     return (
+        <>
+            <Navbar />
             <Component {...pageProps} />
+            </>
     )
 }
